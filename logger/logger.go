16141680fd2c99d840af3a68b731e19c6741670e
aarsakian <aarsakian@gmail.com>
2023-11-23 09:21:43 -0800

    added logging

---
+++ logger/logger.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Logger struct {
+	info    *log.Logger
+	warning *log.Logger
+	error_  *log.Logger
+	active  bool
+}
+
+var VMDKlogger Logger
+
+func InitializeLogger(active bool) {
+	if active {
+		now := time.Now()
+		logfilename := "logs" + now.Format("2006-01-02T15_04_05") + ".txt"
+		file, err := os.OpenFile(logfilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		info := log.New(file, "INFO: ", log.Ldate|log.Ltime)
+		warning := log.New(file, "WARNING: ", log.Ldate|log.Ltime)
+		error_ := log.New(file, "ERROR: ", log.Ldate|log.Ltime)
+		VMDKlogger = Logger{info: info, warning: warning, error_: error_, active: active}
+	} else {
+		VMDKlogger = Logger{active: active}
+	}
+
+}
+
+func (logger Logger) Info(msg string) {
+	if logger.active {
+		logger.info.Println(msg)
+	}
+}
+
+func (logger Logger) Error(msg any) {
+	if logger.active {
+		logger.error_.Println(msg)
+	}
+}
+
+func (logger Logger) Warning(msg string) {
+	if logger.active {
+		logger.warning.Println(msg)
+	}
+}
