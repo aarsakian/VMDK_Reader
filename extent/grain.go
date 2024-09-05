a08d0b5818e291091c6205535283c3c13f9b0948
Armen Arsakian <arsakian@digitalforensics.gr>
2024-09-05 12:11:25 +0300

    added comments/removed redudant code

--- extent/grain.go
+++ extent/grain.go
@@ -10,6 +10,8 @@ type GrainOffsets []int
 //gtCoverage by a grain table nof GTE x grainSize
 //he size of a sparse extent should be a multiple of grainSize
 //each gt is 2kb 512 entries of 32 bit
+//GDE offset in sectors of a grain table in a sparse extent
+//GTE offset of a grain in the sparse extent
 
 func (sparseH SparseHeader) PopulateGrainOffsets(data []byte) GrainOffsets {
 	var gteOffsets GrainOffsets
@@ -18,16 +20,16 @@ func (sparseH SparseHeader) PopulateGrainOffsets(data []byte) GrainOffsets {
 	var startOffset int
 	gtCoverage := sparseH.NumGTEsPerGT * uint32(sparseH.GrainSize) //covered sectors by a single grain table
 	nofGDEntries := int(sparseH.Capacity) / int(gtCoverage)        //required GD entries
-	counter := 0
+
 	for gdEntry := 0; gdEntry < nofGDEntries; gdEntry++ {
-		gtOffset = int(utils.ReadEndianInt(data[gdEntry*4 : gdEntry*4+4]))
+		gtOffset = int(utils.ReadEndianInt(data[gdEntry*4 : gdEntry*4+4])) //grain table offset
 
 		for gtEntry := 0; gtEntry < int(sparseH.NumGTEsPerGT); gtEntry++ {
-			startOffset = gtOffset*512 + gtEntry*4 - int(sparseH.GdOffset)*512
+			startOffset = gtOffset*512 + gtEntry*4 - int(sparseH.GdOffset)*512 //offset to buffer
 			gteOffset = int(utils.ReadEndianInt(data[startOffset : startOffset+4]))
 			//fmt.Printf("%d %d | %d\t", counter, gtOffset*512+gtEntry*4, gteOffset)
 			gteOffsets = append(gteOffsets, gteOffset)
-			counter++
+
 		}
 	}
 	return gteOffsets
